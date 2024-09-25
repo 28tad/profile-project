7bb8d327800748f7688366d2057ee79b2dc6fde3
28tad <alimaxcooll@gmail.com>
2024-09-25 16:07:04 +0300

    add Error Boundary

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import { ThemePorvider } from '@/app/providers/ThemeProvider';
 import '@/shared/config/i18n/i18n';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 render(
     <BrowserRouter>
-        <ThemePorvider>
-            <App />
-        </ThemePorvider>
+        <ErrorBoundary>
+            <ThemePorvider>
+                <App />
+            </ThemePorvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root'),
 );
