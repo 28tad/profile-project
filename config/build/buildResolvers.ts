a87dcda0cda8f43c853529ce5141490ff859654f
28tad <alimaxcooll@gmail.com>
2024-10-01 20:28:40 +0300

    add storybook, create stories for components

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,7 +3,7 @@ import { BuildOptions } from './types/config';
 
 export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
         preferAbsolute: true,
         modules: [options.paths.src, 'node_modules'],
         alias: { '@': options.paths.src },
---
+++ config/storybook/webpack.config.ts
@@ -0,0 +1,40 @@
+import webpack, { RuleSetRule } from 'webpack';
+import path from 'path';
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
+import { BuildPaths } from '../build/types/config';
+
+export default ({ config }: {config: webpack.Configuration}) => {
+    const paths: BuildPaths = {
+        build: '',
+        html: '',
+        entry: '',
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    };
+    config.resolve?.modules?.push(paths.src);
+
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+        ...config.resolve?.alias,
+        '@': path.resolve(paths.src),
+    }
+
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+
+    config.module.rules = config.module?.rules?.map((rule: any) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
+
+        return rule
+    })
+
+    config.resolve.extensions?.push('.ts', '.tsx');
+    config.module.rules.push(buildCssLoader(true));
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    })
+
+    return config;
+};
